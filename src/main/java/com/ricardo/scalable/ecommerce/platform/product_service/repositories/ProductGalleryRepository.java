7e6d4533609bf092d00d8068f49ca10c3e44bf10
RicardoRG96 <ricardoretamal10@gmail.com>
2025-02-12 05:49:30 -0300

    add ProductGalleryRepository

--- src/main/java/com/ricardo/scalable/ecommerce/platform/product_service/repositories/ProductGalleryRepository.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/product_service/repositories/ProductGalleryRepository.java
@@ -1,5 +1,20 @@
 package com.ricardo.scalable.ecommerce.platform.product_service.repositories;
 
-public interface ProductGalleryRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ricardo.scalable.ecommerce.platform.product_service.entities.ProductGallery;
+
+public interface ProductGalleryRepository extends CrudRepository<ProductGallery, Long> {
+
+    Optional<List<ProductGallery>> findByProductId(Long id);
+
+    Optional<List<ProductGallery>> findByColorAttributeId(Long id);
+
+    Optional<ProductGallery> findByProductIdAndColorAttributeId(Long productId, Long colorAttributeId);
+
+    Optional<ProductGallery> findByImageUrl(String imageUrl);
 
 }
