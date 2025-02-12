96da054a5a2010c8b5c558cc15895e1c9725b82b
RicardoRG96 <ricardoretamal10@gmail.com>
2025-02-12 05:45:30 -0300

    add productGallery entity

---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/product_service/entities/ProductGallery.java
@@ -0,0 +1,118 @@
+package com.ricardo.scalable.ecommerce.platform.product_service.entities;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "product_gallery")
+public class ProductGallery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "product_id", unique = false)
+    @ManyToOne
+    @NotNull
+    private Product product;
+
+    @JoinColumn(name = "size_attribute_id", unique = false)
+    @ManyToOne
+    @NotNull
+    private ProductAttribute sizeAttribute;
+
+    @JoinColumn(name = "color_attribute_id", unique = false)
+    @ManyToOne
+    @NotNull
+    private ProductAttribute colorAttribute;
+
+    @NotBlank
+    private String imageUrl;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @CreationTimestamp
+    private Timestamp updatedAt;
+
+    public ProductGallery() {
+    }
+
+    public ProductGallery(Product product, ProductAttribute sizeAttribute, ProductAttribute colorAttribute, String imageUrl) {
+        this.product = product;
+        this.sizeAttribute = sizeAttribute;
+        this.colorAttribute = colorAttribute;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public ProductAttribute getSizeAttribute() {
+        return sizeAttribute;
+    }
+
+    public void setSizeAttribute(ProductAttribute sizeAttribute) {
+        this.sizeAttribute = sizeAttribute;
+    }
+
+    public ProductAttribute getColorAttribute() {
+        return colorAttribute;
+    }
+
+    public void setColorAttribute(ProductAttribute colorAttribute) {
+        this.colorAttribute = colorAttribute;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
