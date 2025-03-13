f87be18f9624748c52ba061d376445ec93b5bd15
RicardoRG96 <ricardoretamal10@gmail.com>
2025-03-13 06:23:21 -0300

    add testCheckOverlappingDiscount to DiscountRepository

--- src/test/java/com/ricardo/scalable/ecommerce/platform/product_service/repositories/DiscountRepositoryTest.java
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/product_service/repositories/DiscountRepositoryTest.java
@@ -2,6 +2,7 @@ package com.ricardo.scalable.ecommerce.platform.product_service.repositories;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -46,6 +47,22 @@ public class DiscountRepositoryTest {
                 NoSuchElementException.class, 
                 () -> invalidDiscount.orElseThrow().getDiscountType()    
             )
+        );
+    }
+
+    @Test
+    void testCheckOverlappingDiscount() {
+        LocalDateTime overlappingDiscountStartDate = LocalDateTime.of(2025, 3, 1, 0, 0, 0);
+        LocalDateTime overlappingDiscountEndDate = LocalDateTime.of(2025, 3, 31, 23, 59, 59);
+        int overlappingDiscount = discountRepository.checkOverlappingDiscount(1L, overlappingDiscountStartDate, overlappingDiscountEndDate);
+
+        LocalDateTime notOverlappingDiscountStartDate = LocalDateTime.of(2025, 2, 1, 0, 0, 0);
+        LocalDateTime notOverlappingDiscountEndDate = LocalDateTime.of(2025, 2, 28, 23, 59, 59);
+        int notOverlappingDiscount = discountRepository.checkOverlappingDiscount(3L, notOverlappingDiscountStartDate, notOverlappingDiscountEndDate);
+
+        assertAll(
+            () -> assertEquals(overlappingDiscount, 2),
+            () -> assertEquals(notOverlappingDiscount, 1)
         );
     }
 
