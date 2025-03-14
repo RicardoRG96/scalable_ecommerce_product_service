ba3aeb8f5242ba39c3cbea581fc2c1791499944d
RicardoRG96 <ricardoretamal10@gmail.com>
2025-03-14 06:12:51 -0300

    add testGetDiscountByProductId to DiscountController

--- src/test/java/com/ricardo/scalable/ecommerce/platform/product_service/integrationTests/DiscountControllerTest.java
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/product_service/integrationTests/DiscountControllerTest.java
@@ -3,9 +3,6 @@ package com.ricardo.scalable.ecommerce.platform.product_service.integrationTests
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -41,7 +38,7 @@ public class DiscountControllerTest {
 
     @Test
     @Order(1)
-    void testFindById() {
+    void testGetDiscountById() {
         client.get()
                 .uri("/discounts/1")
                 .exchange()
@@ -53,11 +50,44 @@ public class DiscountControllerTest {
                         JsonNode json = objectMapper.readTree(res.getResponseBody());
                         assertAll(
                             () -> assertNotNull(json),
-                            () -> assertEquals(1L, json.get("id").asLong()),
-                            () -> assertEquals("fixed_amount", json.get("discountType").asText()),
-                            () -> assertEquals(10.00, json.get("discountValue").asDouble()),
-                            () -> assertEquals("2025-03-01T00:00:00.000-03:00", json.get("startDate").asText()),
-                            () -> assertEquals("2025-03-31T23:59:59.000-03:00", json.get("endDate").asText())
+                            () -> assertEquals(1L, json.path("id").asLong()),
+                            () -> assertEquals("fixed_amount", json.path("discountType").asText()),
+                            () -> assertEquals(10.00, json.path("discountValue").asDouble()),
+                            () -> assertEquals("2025-03-01T00:00:00.000-03:00", json.path("startDate").asText()),
+                            () -> assertEquals("2025-03-31T23:59:59.000-03:00", json.path("endDate").asText())
+                        );
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    @Test
+    @Order(2)
+    void testGetDiscountByProductSkuId() {
+        client.get()
+                .uri("/discounts/product_sku/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .consumeWith(res -> {
+                    try {
+                        JsonNode json = objectMapper.readTree(res.getResponseBody());
+                        assertAll(
+                            () -> assertNotNull(json),
+                            () -> assertTrue(json.isArray()),
+                            () -> assertEquals(3, json.size()),
+                            () -> assertEquals(1L, json.get(0).path("id").asLong()),
+                            () -> assertEquals("fixed_amount", json.get(0).path("discountType").asText()),
+                            () -> assertEquals(10.00, json.get(0).path("discountValue").asDouble()),
+                            () -> assertEquals("2025-03-01T00:00:00.000-03:00", json.get(0).path("startDate").asText()),
+                            () -> assertEquals("2025-03-31T23:59:59.000-03:00", json.get(0).path("endDate").asText()),
+                            () -> assertEquals(4L, json.get(1).path("id").asLong()),
+                            () -> assertEquals("percentage", json.get(1).path("discountType").asText()),
+                            () -> assertEquals(10.00, json.get(1).path("discountValue").asDouble()),
+                            () -> assertEquals("2025-02-01T00:00:00.000-03:00", json.get(1).path("startDate").asText()),
+                            () -> assertEquals("2025-02-28T23:59:59.000-03:00", json.get(1).path("endDate").asText())
                         );
                     } catch (Exception e) {
                         e.printStackTrace();
