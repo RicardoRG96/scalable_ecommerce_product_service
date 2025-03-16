98a0110c1ebac351d9fcc5f79aea16724cb22350
RicardoRG96 <ricardoretamal10@gmail.com>
2025-03-16 08:57:54 -0300

    add testGetAllDiscountCodes to DiscountCodeController

--- src/test/java/com/ricardo/scalable/ecommerce/platform/product_service/integrationTests/DiscountCodeControllerTest.java
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/product_service/integrationTests/DiscountCodeControllerTest.java
@@ -273,6 +273,29 @@ public class DiscountCodeControllerTest {
     }
 
     @Test
+    @Order(13)
+    void testGetAllDiscountCodes() {
+        client.get()
+                .uri("/discount-code")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .consumeWith(res -> {
+                    try {
+                        JsonNode json = objectMapper.readTree(res.getResponseBody());
+                        assertAll(
+                            () -> assertNotNull(json),
+                            () -> assertTrue(json.isArray()),
+                            () -> assertEquals(4, json.size())
+                        );
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    @Test
     void testProfile() {
         String[] activeProfiles = env.getActiveProfiles();
         assertArrayEquals(new String[] { "test" }, activeProfiles);
