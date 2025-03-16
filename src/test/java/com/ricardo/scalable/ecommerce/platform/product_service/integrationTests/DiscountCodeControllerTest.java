c6b099cf2d908c4f58e055f24d6bb97126430e9b
RicardoRG96 <ricardoretamal10@gmail.com>
2025-03-16 08:00:18 -0300

    add DiscountCodeControllerTestData

---
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/product_service/integrationTests/DiscountCodeControllerTest.java
@@ -0,0 +1,49 @@
+package com.ricardo.scalable.ecommerce.platform.product_service.integrationTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import static com.ricardo.scalable.ecommerce.platform.product_service.services.testData.DiscountCodeControllerTestData.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class DiscountCodeControllerTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Autowired
+    private Environment env;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void testProfile() {
+        String[] activeProfiles = env.getActiveProfiles();
+        assertArrayEquals(new String[] { "test" }, activeProfiles);
+    }
+
+    @Test
+    void testApplicationPropertyFile() {
+        assertEquals("jdbc:h2:mem:public;NON_KEYWORDS=value", env.getProperty("spring.datasource.url"));
+    }
+
+}
